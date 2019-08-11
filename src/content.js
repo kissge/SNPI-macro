b9c584e9565e20aa905f57a5695aa930c2d36439
kissge <mail@yo.eki.do>
2019-08-11 17:15:21 +0900

    lint

--- src/content.js
+++ src/content.js
@@ -4,103 +4,103 @@ import Mousetrap from 'mousetrap'
 Mousetrap.prototype.stopCallback = () => false
 
 const countAncestors = (div) => {
-    let i = 0
-    while (div && !div.classList.contains('notion-page-content')) {
-        div = div.parentElement
-        ++i
-    }
-    return i
+  let i = 0
+  while (div && !div.classList.contains('notion-page-content')) {
+    div = div.parentElement
+    ++i
+  }
+  return i
 }
 
 const getParticipantsList = () => {
-    const lines = Array.from(document.querySelectorAll('.notion-page-content [contenteditable]'))
+  const lines = Array.from(document.querySelectorAll('.notion-page-content [contenteditable]'))
 
-    while (lines.length > 0 && !lines[0].textContent.includes('参加者')) {
-        lines.shift()
+  while (lines.length > 0 && !lines[0].textContent.includes('参加者')) {
+    lines.shift()
+  }
+
+  if (lines.length < 2) {
+    return []
+  }
+
+  const headerDepth = countAncestors(lines.shift())
+  const baseDepth = countAncestors(lines[0])
+  const words = []
+  while (lines.length > 0) {
+    const div = lines.shift()
+    const depth = countAncestors(div)
+    if (depth > baseDepth) {
+      continue
+    }
+    if (depth <= headerDepth) {
+      break
     }
 
-    if (lines.length < 2) {
-        return []
+    const word = div.textContent.trim()
+    if (word === '') {
+      break
     }
 
-    const headerDepth = countAncestors(lines.shift())
-    const baseDepth = countAncestors(lines[0])
-    const words = []
-    while (lines.length > 0) {
-        const div = lines.shift()
-        const depth = countAncestors(div)
-        if (depth > baseDepth) {
-            continue
-        }
-        if (depth <= headerDepth) {
-            break
-        }
+    words.push(word)
+  }
 
-        const word = div.textContent.trim()
-        if (word === '') {
-            break
-        }
-
-        words.push(word)
+  return words.map(word => {
+    for (let i = 1; i <= word.length; ++i) {
+      const short = word.substr(0, i)
+      if (words.every(word2 => word === word2 || word2.substr(0, i) !== short)) {
+        return short
+      }
     }
 
-    return words.map(word => {
-        for (let i = 1; i <= word.length; ++i) {
-            const short = word.substr(0, i)
-            if (words.every(word2 => word === word2 || word2.substr(0, i) !== short)) {
-                return short
-            }
-        }
-
-        return word
-    }).map(word => `${word}：`)
+    return word
+  }).map(word => `${word}：`)
 }
 
 const updateName = (id) => {
-    const names = getParticipantsList()
-    let textToInsert
+  const names = getParticipantsList()
+  let textToInsert
 
-    if (typeof id === typeof 0) {
-        if (id > names.length) {
-            return
-        }
-
-        textToInsert = names[id - 1]
+  if (typeof id === typeof 0) {
+    if (id > names.length) {
+      return
     }
 
-    const selection = document.getSelection()
-    let { focusNode, focusOffset } = selection
+    textToInsert = names[id - 1]
+  }
 
-    let current = { name: '', i: -1 }
-    names.forEach((name, i) => {
-        if (selection.anchorNode.textContent.substr(0, name.length) === name) {
-            current = { name, i }
-        }
-    })
+  const selection = document.getSelection()
+  let { focusNode, focusOffset } = selection
 
-    if (id === 'next') {
-        textToInsert = names[current.i === -1 ? 0 : (current.i + 1) % names.length]
-    } else if (id === 'prev') {
-        textToInsert = names[current.i === -1 ? names.length - 1 : (current.i + names.length - 1) % names.length]
+  let current = { name: '', i: -1 }
+  names.forEach((name, i) => {
+    if (selection.anchorNode.textContent.substr(0, name.length) === name) {
+      current = { name, i }
     }
+  })
 
-    selection.setBaseAndExtent(selection.anchorNode, 0, selection.anchorNode, current.name.length)
-    document.execCommand('insertText', true, textToInsert)
+  if (id === 'next') {
+    textToInsert = names[current.i === -1 ? 0 : (current.i + 1) % names.length]
+  } else if (id === 'prev') {
+    textToInsert = names[current.i === -1 ? names.length - 1 : (current.i + names.length - 1) % names.length]
+  }
 
-    if (focusNode.nodeType !== Node.TEXT_NODE) {
-        if (focusNode.firstChild && focusNode.firstChild.nodeType === Node.TEXT_NODE) {
-            focusNode = focusNode.firstChild
-        } else {
-            return
-        }
+  selection.setBaseAndExtent(selection.anchorNode, 0, selection.anchorNode, current.name.length)
+  document.execCommand('insertText', true, textToInsert)
+
+  if (focusNode.nodeType !== Node.TEXT_NODE) {
+    if (focusNode.firstChild && focusNode.firstChild.nodeType === Node.TEXT_NODE) {
+      focusNode = focusNode.firstChild
+    } else {
+      return
     }
+  }
 
-    focusOffset += textToInsert.length - current.name.length
-    selection.setBaseAndExtent(focusNode, focusOffset, focusNode, focusOffset)
+  focusOffset += textToInsert.length - current.name.length
+  selection.setBaseAndExtent(focusNode, focusOffset, focusNode, focusOffset)
 }
 
 for (let i = 1; i <= 9; ++i) {
-    Mousetrap.bind(`ctrl+alt+${i}`, () => updateName(i))
+  Mousetrap.bind(`ctrl+alt+${i}`, () => updateName(i))
 }
 Mousetrap.bind('ctrl+alt+j', () => updateName('next'))
 Mousetrap.bind('ctrl+alt+k', () => updateName('prev'))
